4e46d2d5b03632f03a99d20892e0490b65c69b4a
xunfen <107894831+xunfen@users.noreply.github.com>
2026-07-29 13:35:59 +0800

    Delete teacher-client/dist directory

--- teacher-client/dist/service-worker.js
+++
@@ -1,4 +0,0 @@
-const C = 'keban-teacher-v1'; const URLs = ['/','/setup','/index.html']
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(URLs)));self.skipWaiting()})
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))))})
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>new Response('离线中',{status:503}))))})
